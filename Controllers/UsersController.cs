8e127bbf0fe03bb19fabc84d485999288a562597
Yoel Horvitz <yoelhor@live.com>
2024-01-28 17:47:58 +0200

    Sign-in and call Graph

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System.Dynamic;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Identity.Abstractions;
+
+namespace woodgrove_portal.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    private readonly ILogger<UsersController> _logger;
+    private readonly GraphServiceClient _graphServiceClient;
+
+
+    public UsersController(ILogger<UsersController> logger, IConfiguration configuration, GraphServiceClient graphServiceClient)
+    {
+        _logger = logger;
+        _configuration = configuration;
+        _graphServiceClient = graphServiceClient;
+    }
+
+    [HttpGet("list")]
+    public async Task<IActionResult> UsersListAsync()
+    {
+        var users = await _graphServiceClient.Users.GetAsync();
+        return Ok(users);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+// The following line enables Application Insights telemetry collection.
+//builder.Services.AddApplicationInsightsTelemetry();
+
+// Add authentication for the multi-tenant app and access Microsoft Graph as the user
+// https://learn.microsoft.com/entra/identity-platform/multi-service-web-app-access-microsoft-graph-as-user
+IEnumerable<string>? initialScopes = builder.Configuration["DownstreamApi:Scopes"]?.Split(' ');
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+        .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+                .AddMicrosoftGraph(builder.Configuration.GetSection("DownstreamApi"))
+                .AddInMemoryTokenCaches();
+
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI(); ;
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,8 +41,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
